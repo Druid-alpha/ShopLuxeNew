4148573b0e2ed7ba3ff0fe1077bced5168347b7c
Druid-alpha <oluwafemiajayi103@gmail.com>
2026-03-25 13:00:06 +0100

    fix build error

--- app/(shop)/product/[id]/page.tsx
+++ app/(shop)/product/[id]/page.tsx
@@ -1,2 +1,2 @@
-export { generateMetadata } from "../products/[id]/page";
-export { default } from "../products/[id]/page";
+export { generateMetadata } from "../../products/[id]/page";
+export { default } from "../../products/[id]/page";
